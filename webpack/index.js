ba33346a477ee0c630c88a1fac20028f9d6e5166
liuyanjie <lyj8888888888@gmail.com>
2016-06-15 23:06:50 +0800

    mv index.js to src/index.js

--- webpack/index.js
+++
@@ -1,4 +0,0 @@
-
-require('babel-register'); // 将babel注册到nodejs模块系统中，并开始编译其中require的文件。
-require('./src/app.js');
-
---
+++ webpack/src/index.js
@@ -0,0 +1,5 @@
+
+// 将babel注册到nodejs模块系统中，并开始编译其中require的文件。
+require('babel-register');
+require('./app.js');
+
