59fb7264f2ad267eb40ba4844187e3ebb85d8795
liuyanjie <liuyanjie@gmail.com>
2016-09-01 21:17:29 +0800

    eventloop

--- javascript/syntax/estree.js
+++ javascript/syntax/estree.js
@@ -3,7 +3,29 @@ var escope = require('escope');
 var esprima = require('esprima');
 var estraverse = require('estraverse');
 
-var code = 'var answer = {v1:1, v2:2}; function f(){var a = answer;}';
+var code = `
+var i = 1, j = 2, k = 3;
+function a(o, p, x, q) {
+  var x = 4;
+  console.log(i);
+  function b(r, s) {
+    var i = 11, y = 5;
+    console.log(i);
+    function c(t) {
+      var z = 6;
+      console.log(i);
+    }
+    //函数表达式
+    var d = function () {
+      console.log(y);
+    };
+    c(60);
+    d();
+  }
+  b(40, 50);
+}
+a(10, 20, 30);
+`;
 
 var ast = esprima.parse(code);
 var scopeManager = escope.analyze(ast);
